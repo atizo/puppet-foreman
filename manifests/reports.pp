bf4ee2ca7e6dd7f895fb533b7850fb9166ce76fe
Simon Josi <simon.josi@atizo.com>
2011-10-11 13:22:44 +0200

    use cron:crond

--- manifests/reports.pp
+++ manifests/reports.pp
@@ -4,16 +4,16 @@ class foreman::reports {
     content => template("foreman/foreman-report.rb.erb"),
     owner => puppet, group => puppet, mode => 444;
   }
-  cron { 
+  cron::crond { 
     'expire_old_reports':
       user => $foreman::params::user,
-      environment => "RAILS_ENV=$foreman::params::environment",
+      env => "RAILS_ENV=$foreman::params::environment",
       command => "(cd $foreman::params::app_root && rake reports:expire)",
       hour => 7,
       minute  => 30;
     'daily summary':
       user => $foreman::params::user,
-      environment => "RAILS_ENV=$foreman::params::environment",
+      env => "RAILS_ENV=$foreman::params::environment",
       command => "(cd $foreman::params::app_root && rake reports:summarize)",
       hour => 7,
       minute => 31;
